8d824c56eac8e86e6e7f11ab9f3b0545b0ae96c5
Pedro <pgg1998@gmail.com>
2021-06-01 20:37:10 -0700

    Prefixed environment variable with proper prefix

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -6,7 +6,8 @@ import Navbar from "../components/Navbar";
 import { AuthProvider } from "../context/auth";
 import { ArticlesContextProvider } from "../context/articles";
 
-axios.defaults.baseURL = process.env.API_URL || "http://localhost:3001";
+axios.defaults.baseURL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 
 function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
